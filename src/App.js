3b87a9dfb393a3aa9187c87d6c649a6db5cfe82d
Bryan Donovan <bryan.donovan@gmail.com>
2019-07-14 08:42:10 -0700

    add posenet with camera support

--- src/App.js
+++ src/App.js
@@ -1,24 +1,135 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import * as posenet from '@tensorflow-models/posenet';
 
 function App() {
+  const videoWidth = 600;
+  const videoHeight = 600;
+  const minPoseConfidence = 0.1;
+  const minPartConfidence = 0.5;
+
+  useEffect(() => {
+    async function loadVideo() {
+      const video = await setupCamera();
+
+      // start the video
+      video.play();
+
+      return video;
+    }
+
+    async function setupCamera() {
+      if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        throw new Error(
+          'Browser API navigator.mediaDevices.getUserMedia not available'
+        );
+      }
+
+      const video = document.getElementById('video');
+
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {
+          facingMode: 'user',
+          width: videoWidth,
+          height: videoHeight,
+        },
+      });
+      video.srcObject = stream;
+
+      return new Promise(resolve => {
+        video.onloadedmetadata = () => {
+          resolve(video);
+        };
+      });
+    }
+
+    function detectPoseInRealTime(video, net) {
+      const canvas = document.getElementById('output');
+      const ctx = canvas.getContext('2d');
+
+      canvas.width = videoWidth;
+      canvas.height = videoHeight;
+
+      async function poseDetectionFrame() {
+        const pose = await net.estimateSinglePose(video, {
+          flipHorizontal: true,
+        });
+
+        ctx.clearRect(0, 0, videoWidth, videoHeight);
+        ctx.save();
+        ctx.scale(-1, 1);
+        ctx.translate(-videoWidth, 0);
+        ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+        ctx.restore();
+
+        if (pose.score >= minPoseConfidence) {
+          pose.keypoints.forEach(keypoint => {
+            const { position, score } = keypoint;
+
+            if (score >= minPartConfidence) {
+              const { x, y } = position;
+              ctx.fillStyle = 'blue';
+              ctx.fillRect(x - 3, y - 3, 6, 6);
+            }
+          });
+        }
+
+        window.requestAnimationFrame(poseDetectionFrame);
+      }
+
+      poseDetectionFrame();
+    }
+
+    async function loadModel() {
+      const net = await posenet.load({
+        architecture: 'MobileNetV1',
+        outputStride: 16,
+        inputResolution: 513,
+        multiplier: 0.75,
+      });
+
+      let video;
+
+      try {
+        video = await loadVideo();
+
+        const canvas = document.getElementById('output');
+        const ctx = canvas.getContext('2d');
+
+        ctx.clearRect(0, 0, videoWidth, videoHeight);
+
+        ctx.save();
+        ctx.scale(-1, 1);
+        ctx.translate(-videoWidth, 0);
+        ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+        ctx.restore();
+      } catch (e) {
+        // [todo] update the state error
+        throw e;
+      }
+
+      detectPoseInRealTime(video, net);
+    }
+
+    loadModel();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <video
+        id="video"
+        playsInline
+        height={videoHeight}
+        width={videoWidth}
+        style={{
+          MozTransform: 'scaleX(-1)',
+          OTransform: 'scaleX(-1)',
+          WebkitTransform: 'scaleX(-1)',
+          transform: 'scaleX(-1)',
+          display: 'none',
+        }}
+      />
+      <canvas id="output" height={videoHeight} width={videoWidth} />
     </div>
   );
 }
