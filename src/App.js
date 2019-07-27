2916b933d42b2464ed6ac4da3456e74f12b43182
Bryan Donovan <bryan.donovan@gmail.com>
2019-07-27 14:51:07 -0700

    add vectorize canvas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import Navbar from './Navbar';
 import Glasses from './Glasses';
+import Vectorize from './Vectorize';
 import Video from './Video';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -17,6 +18,7 @@ function App() {
     <div className="container-fluid vh-100 d-flex flex-column ">
       <Navbar />
       <Video height={500} width={500} onPoseChange={handlePoseChange}>
+        <Vectorize height={500} hud={true} pose={pose} width={500} />
         <Glasses height={500} width={500} pose={pose} />
       </Video>
     </div>
