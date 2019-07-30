0fc4b6d5795beddaeb3e5e75867173e3689304f1
Bryan Donovan <bryan.donovan@gmail.com>
2019-07-29 18:36:22 -0700

    apply prettier formatting

--- src/Glasses/Glasses.jsx
+++ src/Glasses/Glasses.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 export default ({ height, pose, width }) => {
   const imageHeight = 208;
@@ -8,19 +8,19 @@ export default ({ height, pose, width }) => {
   const [glasses, setGlasses] = useState(null);
 
   useEffect(() => {
-    const canvas2 = document.getElementById("glasses");
-    const _context = canvas2.getContext("2d");
-    const _glasses = document.getElementById("image-1");
+    const canvas2 = document.getElementById('glasses');
+    const _context = canvas2.getContext('2d');
+    const _glasses = document.getElementById('image-1');
 
     setContext(_context);
     setGlasses(_glasses);
   }, []);
 
   if (context && pose) {
-    const rightEye = pose.keypoints.find(p => p.part === "rightEye");
-    const leftEye = pose.keypoints.find(p => p.part === "leftEye");
-    const leftEar = pose.keypoints.find(p => p.part === "leftEar");
-    const rightEar = pose.keypoints.find(p => p.part === "rightEar");
+    const rightEye = pose.keypoints.find(p => p.part === 'rightEye');
+    const leftEye = pose.keypoints.find(p => p.part === 'leftEye');
+    const leftEar = pose.keypoints.find(p => p.part === 'leftEar');
+    const rightEar = pose.keypoints.find(p => p.part === 'rightEar');
 
     context.clearRect(0, 0, height, width);
 
@@ -79,9 +79,9 @@ export default ({ height, pose, width }) => {
         id="glasses"
         height={height}
         width={width}
-        style={{ zIndex: 2, position: "absolute" }}
+        style={{ zIndex: 2, position: 'absolute' }}
       />
-      <div style={{ display: "none" }}>
+      <div style={{ display: 'none' }}>
         <img id="image-1" src="ShutterGlasses.svg" alt="glasses" />
       </div>
     </>
