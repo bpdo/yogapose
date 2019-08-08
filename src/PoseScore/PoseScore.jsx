de121a107d3bb4fd2ced208532355e80a3e5fcb3
Andy Ruestow <andrew.d.ruestow@lmco.com>
2019-08-08 16:51:06 -0400

    Add Background to ScoreOverlay

--- src/PoseScore/PoseScore.jsx
+++ src/PoseScore/PoseScore.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import PoseScore from '../static/ScoreOverlay.svg';
 
+const textStyle = {
+  color: 'white',
+};
 export default ({ score }) => {
-  return <h1>{score}</h1>;
+  return (
+    <div className='text-center p-0 d-flex align-items-center'>
+      <img className='img-fluid position-relative' src={PoseScore} />
+      <h2 className='w-25 position-absolute' style={textStyle}>
+        {score}
+      </h2>
+    </div>
+  );
 };
