73825bf3b249cf43012ca2a491d84731272aef4a
Bryan Donovan <bryan.donovan@gmail.com>
2019-08-08 16:09:55 -0700

    refactor controls layout

--- src/Controls/Controls.jsx
+++ src/Controls/Controls.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import Start from '../static/Start.svg';
-import Reset from '../static/Reset.svg';
-import Submit from '../static/Submit.svg';
+import Start from './Start.svg';
+import Reset from './Reset.svg';
+import Submit from './Submit.svg';
+import styles from './Controls.css';
 
 export default () => {
   return (
-    <div className='text-center p-0 d-flex'>
-      <img className='img-fluid position-relative' src={Start} />
-      <img className='img-fluid position-relative' src={Reset} />
-      <img className='img-fluid position-relative' src={Submit} />
+    <div className='d-flex'>
+      <img
+        className='img-fluid position-relative'
+        src={Start}
+        alt='Start'
+        style={styles.button}
+      />
+      <img
+        className='img-fluid position-relative'
+        src={Reset}
+        alt='Reset'
+        style={styles.button}
+      />
+      <img
+        className='img-fluid position-relative'
+        src={Submit}
+        alt='Submit'
+        style={styles.button}
+      />
     </div>
   );
 };
